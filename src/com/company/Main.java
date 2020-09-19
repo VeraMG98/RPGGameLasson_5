1b7ea5c9776395acd753859f5882da0388205725
macbook <rusyamg@mail.ru>
2020-09-19 22:12:23 +0600

    Исправила возвращение массива в методе heroCreate

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 	    bossCreate();
-	    Hero[] heroes = new Hero[3];
+	    Hero[] heroes = heroCreate();
         for (int i = 0; i < heroes.length; i++) {
-            System.out.println(heroCreate(i).getHealth() + " " + heroCreate(i).getDamage()
-                    + " " + heroCreate(i).getAttackType());
+            System.out.println(heroes[i].getHealth() + " " + heroes[i].getDamage()
+                    + " " + heroes[i].getAttackType());
         }
+       /* BossClass boss = new BossClass();
+        boss.setHealth(500);
+        boss.setDamage(50);
+        Weapon bossWeapon = new Weapon();
+        bossWeapon.setNameWeapon("Sword");
+        bossWeapon.setTypeWeapon("Physical");
+        boss.setWeapon(bossWeapon);
+
+        System.out.println("Boss health " + boss.getHealth() + ", damage "
+                + boss.getDamage() + ", weapon " + boss.getWeapon().getNameWeapon() + " "
+                + boss.getWeapon().getTypeWeapon());*/
     }
 
-    public static Hero heroCreate(int count) {
+    public static Hero[] heroCreate() {
         Hero firstHero = new Hero(260, 20, "Physical");
         Hero secondHero = new Hero(250, 25, "Magical");
         Hero thirdHero = new Hero(200, 0);
         Hero[] heroes = new Hero[] {firstHero, secondHero, thirdHero};
-        return heroes[count];
+        return heroes;
     }
 
     public static void bossCreate() {
